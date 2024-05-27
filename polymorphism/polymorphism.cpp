5cc0c64288150bf1e2f6ae67a3531cb4ecb0ffec
KamiliaNurmala <kamilia.nurmala.ft23@mail.umy.ac.id>
2024-05-27 10:00:49 +0700

    membuat class parent "seseorang"

--- polymorphism/polymorphism.cpp
+++ polymorphism/polymorphism.cpp
@@ -1,2 +1,13 @@
 #include <iostream>
 using namespace std;
+
+class seseorang {
+public:
+	virtual void pesn() = 0;
+	/*
+	virtual void pesan(){
+		cout << "Pesan dari seseorang" << endl;
+	}
+	*/
+};
+
