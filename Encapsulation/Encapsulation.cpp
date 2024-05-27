00c9d18c0dc489459464bb7080b1587aaa26e0a5
KamiliaNurmala <kamilia.nurmala.ft23@mail.umy.ac.id>
2024-05-27 10:14:23 +0700

    memasukkan parameter pada method objek

--- Encapsulation/Encapsulation.cpp
+++ Encapsulation/Encapsulation.cpp
@@ -17,4 +17,12 @@ public:
 
 int main() {
 	remoteLampu lampuRumah;
+
+	lampuRumah.setSaklarNo(0, "Lampu Teras Rumah");
+	lampuRumah.setSaklarNo(1, "Lampu Ruang Tamu");
+	lampuRumah.setSaklarNo(2, "Lampu Kamar Tidur");
+	lampuRumah.setSaklarNo(0, "Lampu Dapur");
+
+
+
 }
